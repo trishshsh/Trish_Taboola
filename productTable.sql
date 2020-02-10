1a56410fcc79187654c80a11d402f977d6a4ad38
Trish Lam <40235462+trishshsh@users.noreply.github.com>
2020-02-09 23:42:11 -0800

    add commenting to sql file

--- productTable.sql
+++ productTable.sql
@@ -1,8 +1,16 @@
+--Create Table for Products, including strings: the product name as the
+--primary key and the category in which the product is in.
 CREATE TABLE Products (
     product_name VARCHAR(100) PRIMARY KEY,
     category VARCHAR(100)
 );
 
+
+
+--Create Table for the Price History of the Products
+--Includes a History ID in order to uniquely define the different rows
+--Product name which is the foreign key that refers back to the Products table
+--Price, begin_date, end_date which describe the product
 CREATE TABLE Price_History (
     history_id INT PRIMARY KEY,
     product_name VARCHAR(100) NOT NULL,
@@ -12,23 +20,48 @@ CREATE TABLE Price_History (
     FOREIGN KEY(product_name) REFERENCES Products(product_name) ON DELETE CASCADE
 );
 
+
+
+--Insert instances for Products table: Basketball and Laptop
 INSERT INTO Products
 VALUES ('Basketball', 'Sports');
 INSERT INTO Products
 VALUES ('Laptop', 'Electronics');
 
-INSERT INTO Price_History
-VALUES (1, 'Basketball', 10.99, 2020-1-07, NULL);
-INSERT INTO Price_History
-VALUES (2, 'Laptop', 1250.00, 2020-1-07, NULL);
-INSERT INTO Price_History
-VALUES (3, 'Basketball', 9.99, 2020-1-09, NULL);
 
+
+--Insert instances for Price_History of the products added
+--into the Products table: Basketball and Laptop.
+--The end dates for the products are set to NULL,
+--because the current price doed not have an end date.
+INSERT INTO Price_History
+VALUES (1,'Basketball', 10.99, '2020-01-07', NULL);
+INSERT INTO Price_History
+VALUES (2,'Laptop', 1250.00, '2020-01-07', NULL);
+
+
+
+--Basketball gets added into Price History again because
+--its price has been changed.
+INSERT INTO Price_History
+VALUES (3,'Basketball', 9.99, '2020-01-11', NULL);
+
+
+
+--Updates the end date for the old Basketball row, since its
+--price has changed and thus is not the current price.
 UPDATE Price_History 
-SET Price_History.end_date = (SELECT MAX(begin_date) FROM Price_History)
+--Selects latest begin date because it is the date in which
+--the current price has changed
+SET end_date = (SELECT MAX(begin_date) FROM Price_History)
 WHERE Price_History.product_name = 'Basketball'
-AND Price_History.end_date = NULL;
+AND Price_History.end_date IS NULL
+--To ensure that the current price does not have an end date
+AND Price_History.begin_date < (SELECT MAX(begin_date) FROM Price_History);
 
+
+
+--Combines both tables and its columns using the product name
 SELECT Products.product_name,
         Products.category,
         Price_History.price,
